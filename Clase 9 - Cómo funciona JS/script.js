650ef2e1c8615254c45866139747b822f5b8cb68
Juan Diego <dippons78@gmail.com>
2019-11-04 22:19:40 -0500

    Actualizacion clase 11

---
+++ Clase 9 - Cómo funciona JS/script.js
@@ -0,0 +1,12 @@
+//Manera en la que agregan comentarios
+/**
+Otra manera de definir comentarios
+ */
+//Manera de definir variables
+var a=1;
+var b=2;
+var nombre="Juan Diego"
+var apellido="Jáuregui"
+//Para concatenar en JS se usa el caracter '+'
+alert(nombre + " " + apellido)
+
